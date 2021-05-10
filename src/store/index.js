d0ec8ab1b422d869381850fe18e534ce33fdef9d
lijunshuang <ljs0904@126.com>
2021-05-10 10:17:09 +0800

    fix:commit

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,8 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    // 参数1 是vuex传递的上下文 context:{ commit, dispatch, state}
+    // 模拟登录api调用,1分钟以后 如果用户名是admin 则调用成功
   },
   modules: {
   }
