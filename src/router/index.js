46367ccb1d06060505249a7953a9103592eade66
lijunshuang <ljs0904@126.com>
2021-05-12 16:04:04 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,8 @@ const routes = [{
     name: 'about',
     component: () => import('../views/About.vue'),
     meta: {
-      auth: true // 需要守卫
+      auth: true, // 需要守卫
+      title:'登录'
     },
   },
   {
@@ -37,35 +38,36 @@ const router = new VueRouter({
 })
 
 // 路由守卫
-// router.beforeEach((to, from, next) => {
-//   if (to.meta.auth) {
-//     // 判断是否登录，如果没有登录 跳转到登录页
-//     if (store.isLogin) {
-//       next()
-//     } else {
-//       next(`/login?redirect=${to.fullPath}`)
-//     }
-//   } else {
-//     next()
-//   }
-// })
-
-// 全局守卫修改为：要求用户必须登录，否则只能去登录页
 router.beforeEach((to, from, next) => {
-  if (store.state.isLogin) {
-    if (to.path === '/login') {
-      next('/')
-    } else {
+  document.title = to.meta.title || ''
+  if (to.meta.auth) {
+    // 判断是否登录，如果没有登录 跳转到登录页
+    if (store.isLogin) {
       next()
+    } else {
+      next(`/login?redirect=${to.fullPath}`)
     }
   } else {
-    if (to.path === '/login') {
-      next()
-    } else {
-      next('/login?redirect=' + to.fullPath)
-    }
+    next()
   }
 })
+
+// 全局守卫修改为：要求用户必须登录，否则只能去登录页
+// router.beforeEach((to, from, next) => {
+//   if (store.state.isLogin) {
+//     if (to.path === '/login') {
+//       next('/')
+//     } else {
+//       next()
+//     }
+//   } else {
+//     if (to.path === '/login') {
+//       next()
+//     } else {
+//       next('/login?redirect=' + to.fullPath)
+//     }
+//   }
+// })
 
 
 
